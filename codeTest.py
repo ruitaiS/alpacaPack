d666f4c2787e9972037b389418a539f9b5c9ad67
ruitaiS <shaoruitai@gmail.com>
2020-11-30 12:25:16 -0500

    understanding pandas time

--- codeTest.py
+++ codeTest.py
@@ -1,5 +1,6 @@
 from data import Data
 from datetime import datetime, date
+import pandas as pd
 
 #Login Data
 key_id = 'PKHGR6CVRK7DTWFIB6Q1'
@@ -13,12 +14,23 @@ secret_key = '2h0JwR4hoebs9udjZOtfzCrwlupXBu1MibK6AfrZ'
 base_url = 'https://api.alpaca.markets'
 '''
 
+#today = date.today().strftime("%Y-%m-%d")
+#open_time = str(datetime.now().strftime("%Y-%m-%dT09:30:00"))
+#current_time = str(datetime.now().strftime("%Y-%m-%dT%H:%M:%S"))
+
 def time():
-    print("Date: " + date.today().strftime("%Y-%m-%d"))
-    print("ISO Format Time: " + str(datetime.now().strftime("%Y-%m-%dT%H:%M:%S")))
+    #Date
+    print(pd.Timestamp.now().date())
+
+    #Current time in ISO format:
+    print(pd.Timestamp.now().isoformat())
+
+    #Current time as epoch:
+    print(pd.Timestamp.now().timestamp())
 
 def testData():
     data = Data(key_id, secret_key)
-    data.get_date_bars('TSLA', )
+    data.get_date_bars('TSLA', open_time, current_time)
 
 time()
+#testData()
