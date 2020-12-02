f914a3f26afa68c9ca9594a199c232442af288a2
ruitaiS <shaoruitai@gmail.com>
2020-12-02 14:19:54 -0500

    stream js gets data! now to update it on-screen..

--- frontend/src/Components/stream.js
+++ frontend/src/Components/stream.js
@@ -4,18 +4,30 @@ class Stream extends Component {
     constructor(props){
         super(props);
         this.state = {
-            //TODO: Fill With key_id and secret_key
+            key_id: 'PKHGR6CVRK7DTWFIB6Q1',
+            secret_key: 'TpSauKJD8We5hu3vvXzwp2o7UrXBfR4uzxp4Z27n',
         }
     }
 
     // instance of websocket connection as a class property
     ws = new WebSocket('wss://data.alpaca.markets/stream')
-    out = "hi"
 
     componentDidMount() {
         this.ws.onopen = () => {
         // on connecting, do nothing but log it to the console
         console.log('connected')
+        let auth_data = {
+            "action": "authenticate",
+            "data": {"key_id": this.state.key_id, "secret_key": this.state.secret_key}
+        }
+    
+        this.ws.send(JSON.stringify(auth_data))
+
+        let listen_message = {"action": "listen", "data": {"streams": ["T.TSLA"]}}
+
+        this.ws.send(JSON.stringify(listen_message))            
+
+
         }
 
         this.ws.onmessage = evt => {
@@ -34,7 +46,7 @@ class Stream extends Component {
     }
 
     render(){
-        return("hi")
+        return("Hi")
     }
 }
 
