23b3ba2770efd68bd6a95290118111b345eeb480
ruitaiS <shaoruitai@gmail.com>
2020-12-09 01:52:18 -0500

    why isn't the width changing?

--- frontend/src/Components/healthBar.js
+++ frontend/src/Components/healthBar.js
@@ -27,12 +27,29 @@ class HealthBar extends Component{
         }
     }
 
+    styling(base, curr){
+        let netChange = curr-base
+        let pctChange = 100*netChange / base
+        let width = pctChange + "%"
+
+        if (netChange > 0){
+            //Positive Change
+            return{height: "50px", width: {netChange}, backgroundColor: "green"}
+        }else{
+            //Negative Change
+            return{height: "50px", width: "10%", backgroundColor: "red"}
+        }
+
+        return {transform: "translate(-33%, 0%)", height: "50px", width: "300%", backgroundColor: this.bgColor(curr - base)}
+    }
+
     render(){
         return(
             <div>
                 <div>{this.props.basePrice}</div>
                 <div>{this.props.currPrice}</div>
-                <div style={{width: "100%", backgroundColor: this.bgColor(this.props.currPrice - this.props.basePrice)}}>Percentage Change: {100*(this.props.currPrice - this.props.basePrice)/this.props.basePrice} %</div>
+                <div style={this.styling(this.props.basePrice, this.props.currPrice)}></div>
+                <div>Percentage Change: {100*(this.props.currPrice - this.props.basePrice)/this.props.basePrice} %</div>
             </div>
         )
         
