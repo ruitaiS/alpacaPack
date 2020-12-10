9052fa256bb723a44ff36ceb8a4f39ea1c2540eb
ruitaiS <shaoruitai@gmail.com>
2020-12-10 11:34:45 -0500

    added component template & started work on slider

---
+++ frontend/src/Components/slider.js
@@ -0,0 +1,29 @@
+import {Component} from "react";
+
+class Slider extends Component{
+    constructor(props){
+        super(props);
+        //Values that get passed to the component
+
+        //Functions that need to get bound to this instance
+        //this.function = this.function.bind(this);
+        this.state = {
+            //Default state initialization
+        }
+    }
+
+    function(){
+        console.log("Hi! :)")
+    }
+
+    render(){
+        return(
+            <div>
+                Stuff Goes Here
+            </div>
+        )
+        
+    }
+}
+
+export default ComponentName;
