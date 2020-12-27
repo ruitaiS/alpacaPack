095ee6daf3cd44f86c969ab7410eb8e44eea532e
ruitaiS <shaoruitai@gmail.com>
2020-12-27 01:14:23 -0500

    readying for api testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
   {/*return <Control/>;*/}
-  return <API/>;
+  return <API key_id="PKHGR6CVRK7DTWFIB6Q1" secret_key="TpSauKJD8We5hu3vvXzwp2o7UrXBfR4uzxp4Z27n" ticker="TSLA" side="buy" quantity="1" price=""/>;
 }
 
 export default App;
--- frontend/src/Components/apiHandler.js
+++ frontend/src/Components/apiHandler.js
@@ -3,8 +3,23 @@ import React, { Component } from 'react'
 class API extends Component {
   constructor(props) {
     super(props)
+    this.buy = this.buy.bind(this)
+    this.sell = this.sell.bind(this)
+    this.cancel = this.cancel.bind(this)
+    this.state = {
+      outputText: "",
+      serverText: "",  
   }
-  componentWillMount() {
+
+    //Props:
+    //key_id
+    //secret_key
+    //ticker
+    //side
+    //quantity
+    //price
+  }
+  componentDidMount() {
     this.getData()
   }
 
@@ -23,10 +38,41 @@ class API extends Component {
     xhr.send()
   }
 
+  buy(){
+      
+      let xhr = new XMLHttpRequest()
+
+      xhr.addEventListener('load', () => {
+        // update the state of the component with the result here
+        console.log(xhr.responseText)
+      })
+
+      xhr.open('GET', 'https://dog.ceo/api/breeds/list/all')
+
+      this.setState({outputText: "Buy Submitted"})
+
+  }
+
+  sell(){
+    this.setState({outputText: "Sell Submitted"})
+  }
+
+  cancel(){
+    this.setState({outputText: "Cancelled"})
+  }
+
+
   render() {
+    //Test API call functionality using buttons
     return (
-      <div>
-        <p>Hello World</p>
+      <div className="centered">
+        {this.state.serverText}
+        {this.state.outputText}
+        <div className="row">
+          <div><button className="bigBtn" onClick={this.buy}>Buy</button></div>
+          <div><button className="bigBtn" onClick={this.sell}>Sell</button></div>
+          <div><button className="bigBtn" onClick={this.cancel}>Cancel</button></div>
+        </div>
       </div>
     )
   }
