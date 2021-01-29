a64451c55b6e5f1377b73b653cf391996b99447c
ruitaiS <shaoruitai@gmail.com>
2021-01-29 10:00:53 -0500

    fixed disappearing decimal flicker

--- frontend/src/Components/list.js
+++ frontend/src/Components/list.js
@@ -30,7 +30,7 @@ class List extends Component{
 
         return(
             <fieldset className="portfolioElement">
-              <legend>{symbol} // ${Math.floor(price*100)/100} // {(change*100).toFixed(2)}%</legend>
+              <legend>{symbol} // ${(Math.floor(price*100)/100).toFixed(2)} // {(change*100).toFixed(2)}%</legend>
               <PCTBar height="5" width="300" pctChange={change} />
 
               <div>
