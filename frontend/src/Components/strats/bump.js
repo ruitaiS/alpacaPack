648644fcd69012438087d15c1c5f35c23c5419fc
ruitaiS <shaoruitai@gmail.com>
2021-02-21 10:04:10 -0500

    typo

--- frontend/src/Components/strats/bump.js
+++ frontend/src/Components/strats/bump.js
@@ -10,6 +10,7 @@ class BumbStrat extends Component{
     constructor(props){
         super(props);
         //Values that get passed to the component
+        //
 
         //Functions that need to get bound to this instance
         //this.function = this.function.bind(this);
